1ae26829c5bbdfe1f7ca82713964d0b1059a52e6
Alex <alexandroguerrero02@iesamachado.org>
2024-01-26 14:29:31 +0100

    Viernes 26 de enero de 2024

--- Segundo trimestre/tema3/NumerosArray.js
+++ Segundo trimestre/tema3/NumerosArray.js
@@ -1,4 +1,34 @@
 var numeros = [];
+
+function add() {
+    var n = parseFloat(document.getElementById("numeros").value);
+    numbers.push(numeros);
+    document.getElementById("resultado").innerHTML = numbers
+}
+
+var s=0
+//La variable global solo se inicializa una vez (ejecuta una vez)
+
+function sumar(){
+    s = 0;
+    for (var i = 0 ; i< Numbers.length; i++) {
+        s += numbers[i];
+    }
+    document.getElementById("resultado").innerHTML = s;
+}
+
+function Multiplicar(){
+    var m = 1;
+    for (var i = 0 ; i< numbers.length; i++) {
+        m*= numbers[i];
+    }
+    document.getElementById("resultado").innerHTML = m;
+}
+
+//i++ incrementa un numero
+//for = se repite
+
+/*
 function pintarArray(){
    document.getElementById("numerosanadir").innerHTML = ""; 
 }
@@ -21,3 +51,4 @@ function multiplicar() {
     var producto = document.getElementById("numerosanadir").value
     document.getElementById("resultado").innerHTML = "El resultado es" + producto ;
 }
+*/
