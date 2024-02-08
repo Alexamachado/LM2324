f8143eb62ec94f0c500b1194142f8ff44f70d40e
Alex <alexandroguerrero02@iesamachado.org>
2024-02-08 23:37:54 +0100

    Jueves 8 de febrero de 2024 V2

--- Segundo trimestre/tema3/mayor.js
+++ Segundo trimestre/tema3/mayor.js
@@ -1,8 +1,17 @@
-function mayor(vector){
-    let mayor=vector[0]; //por defector el primer elemento es mayor
-    for (var i=0;i<vector.length;i++){
-        if (vector[i]>mayor)
-        mayor = vector[i];
-    } 
-    return mayor;
+var vector = [];
+
+function anadir(){
+    var n = document.getElementById("numeros").value;
+    vector.push(n)
+    document.getElementById("mostrarnumeros").innerHTML = vector;
 }
+//function mayor(vector){
+function mayor(){
+    let mayorn=vector[0]; //por defecto el primer elemento es mayor
+    for (var i=0;i<vector.length;i++){
+        if (vector[i]>mayorn)
+        mayorn = vector[i];
+    } 
+    document.getElementById("numeromayor").innerHTML = mayorn;
+}
+
